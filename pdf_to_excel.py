ff13b33cff7da46b1c7ab6e065457907a883093d
SeanOnamade <100744222+SeanOnamade@users.noreply.github.com>
2024-09-29 17:58:35 -0500

    updated pdf_to_excel and added example security report and resulting xlsx

--- pdf_to_excel.py
+++ pdf_to_excel.py
@@ -5,11 +5,15 @@ import pandas as pd
 def pdf_to_excel(pdf_file_path, excel_file_path):
     # Read PDF file
     tables = tabula.read_pdf(pdf_file_path, pages='all')
+    if tables:
+        print("Tables located, printing...")
 
-    # Write each table to a separate sheet in the Excel file
-    with pd.ExcelWriter(excel_file_path) as writer:
-        for i, table in enumerate(tables):
-            table.to_excel(writer, sheet_name=f'Sheet{i+1}')
+        # Write each table to a separate sheet in the Excel file
+        with pd.ExcelWriter(excel_file_path) as writer:
+            for i, table in enumerate(tables):
+                table.to_excel(writer, sheet_name=f'Sheet{i+1}')
+    else:
+        print("No tables found in your pdf")
 
 
-pdf_to_excel('path_to_pdf_file.pdf', 'path_to_excel_file.xlsx')
+pdf_to_excel('annual-security-report.pdf', 'vscpolicy.xlsx')
